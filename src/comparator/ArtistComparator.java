5dc671f6ca12f2799266e663c60761bf5944b0e8
Vincent Pierce <vinhpierce@gmail.com>
2022-11-06 15:37:31 -0800

    Done

--- src/comparator/ArtistComparator.java
+++ src/comparator/ArtistComparator.java
@@ -7,6 +7,8 @@ import java.util.Comparator;
 public class ArtistComparator implements Comparator<Song> {
     @Override
     public int compare(Song song1, Song song2) {
-        return song1.getArtist().compareTo(song2.getArtist());
+        String artist1 = song1.getArtist();
+        String artist2 = song2.getArtist();
+        return artist1.compareTo(artist2);
     }
 }
